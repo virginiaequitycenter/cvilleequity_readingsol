f5ebcb46dfff1856ea826e5cca536fa26c1c4b99
Savvy Holmes <seh5uz@virginia.edu>
2020-07-12 14:05:22 -0400

    Update EC Data Set.R

--- EC Data Set.R
+++ EC Data Set.R
@@ -7,6 +7,7 @@ library(sf)
 library(dplyr)
 library(readr)
 library(stringr)
+library(tidyverse)
 setwd("/Users/savannahholmes/Desktop/GitHub/cvilleequity_readingsol")
 
 
@@ -23,7 +24,6 @@ disabled <- read.csv("disabled.csv")
 ------------------------------------------------------------------
 #Pass Rates by State 
 
-write_csv(division,path= 'division')
 
 #State Data
 state <- educationgap %>% select('Division.Number')
@@ -35,80 +35,89 @@ state <- state %>% filter('Pass.Rate'>50)
 
 ------------------------------------------------------------------
 #Pass Rates by Division 
+write_csv(division,path = 'division.csv') 
   
 #Albemarle
-div_alb <- division %>% filter(Division.Number == '2')
-div_alb <- div_alb %>% spread(Race,Pass.Rate)
+div_alb <- division %>% 
+  filter(Division.Number == '2') %>% 
+  spread(Race,Pass.Rate)
 
 
 #Charlottesville 
-div_cville <- division %>% filter(Division.Number == '104')
-div_cville <- division %>% spread(Race,Pass.Rate)
-div_cville <- division %>% filter(Division.Number =='104')
+div_cville <- division %>% 
+  filter(Division.Number == '104') %>% 
+  spread(Race,Pass.Rate) %>% 
 
 
 #Fluvanna
-div_fluv <- division %>% filter(Division.Name == 'Fluvanna County')
-div_fluv <- div_fluv %>% spread(Race, Pass.Rate)
+div_fluv <- division %>% 
+  filter(Division.Name == 'Fluvanna County') %>% 
+  spread(Race, Pass.Rate)
 
 
 #Greene
-div_greene <- division
-div_greene <- division %>% filter(Division.Name == 'Greene County')
-div_greene <- div_greene %>% spread(Race, Pass.Rate)
+div_greene <- division %>% 
+  filter(Division.Name == 'Greene County') %>% 
+  spread(Race, Pass.Rate)
 
 
 #Louisa
-div_louisa <- division %>% filter(Division.Name == 'Louisa County')
-div_louisa <- div_louisa %>% spread(Race, Pass.Rate)
+div_louisa <- division %>% 
+  filter(Division.Name == 'Louisa County') %>% 
+  spread(Race, Pass.Rate)
 
 
 #Nelson
-div_nelson <- division %>% filter(Division.Name == 'Nelson County')
-div_nelson <- div_nelson %>% spread(Race, Pass.Rate)
+div_nelson <- division %>% 
+  filter(Division.Name == 'Nelson County') %>% 
+  spread(Race, Pass.Rate)
 
 
 #Pass Rates by School
-school_allrace <- school_allrace %>% filter(Level == 'School')
-school
-school_blackwhite <- school_blackwhite %>% filter(Level == 'School')
+school_allrace <- school_allrace %>% 
+  filter(Level == 'School') %>% 
 
 
 #Albemarle
 school_alb <- school_allrace %>% filter(Division.Number == '2')
-school_alb_race <- school_blackwhite %>% filter(Division.Number =='2')
-school_alb_race <- school_alb_race %>% spread(Race,Pass.Rate)
+school_alb_race <- school_blackwhite %>% 
+  filter(Division.Number =='2') %>% 
+  spread(Race,Pass.Rate)
 
 
 #Charlottesville
 school_cville <- school_allrace %>% filter(Division.Number == '104')
-school_cville_race <- school_blackwhite %>% filter(Division.Number == '104')
-school_cville_race <- school_cville_race %>% spread(Race,Pass.Rate)
+school_cville_race <- school_blackwhite %>% 
+  filter(Division.Number == '104') %>% 
+  spread(Race,Pass.Rate)
 
 
 #Fluvanna
 school_fluv <- school_allrace %>% filter(Division.Number == '32')
-school_fluv_race <- school_blackwhite %>% filter(Division.Number == '32')
-school_fluv_race <- school_fluv_race %>% spread(Race,Pass.Rate)
+school_fluv_race <- school_blackwhite %>% 
+  filter(Division.Number == '32') %>% 
+  spread(Race,Pass.Rate)
 
 
 #Greene
-school_greene <- school
 school_greene <- school_allrace %>% filter(Division.Number == '39')
-school_greene_race <- school_blackwhite %>% filter(Division.Number== '39')
-school_greene_race <- school_greene_race %>% spread(Race,Pass.Rate)
+school_greene_race <- school_blackwhite %>% 
+  filter(Division.Number== '39') %>% 
+  spread(Race,Pass.Rate)
 
 
 #Louisa
 school_louisa <- school_allrace %>% filter(Division.Number == '54')
-school_louisa_race <- school_allrace %>% filter(Division.Number == '54')
-school_louisa_race <- school_louisa_race %>% spread(Race,Pass.Rate)
+school_louisa_race <- school_allrace %>% 
+  filter(Division.Number == '54') %>% 
+  spread(Race,Pass.Rate)
 
 
 #Nelson
 school_nelson <- school_allrace %>% filter(Division.Number == '62')
-school_nelson_race <- school_blackwhite %>% filter(Division.Number == '62')
-school_nelson_race <- school_nelson_race %>% spread(Race,Pass.Rate)
+school_nelson_race <- school_blackwhite %>% 
+  filter(Division.Number == '62') %>% 
+  spread(Race,Pass.Rate)
 
 ------------------------------------------------------------------
 #Achievement Gap
