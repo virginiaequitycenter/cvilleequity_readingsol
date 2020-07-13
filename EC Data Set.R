5e2376f81ee11ed141c05b5d6f690d3d3b978173
eu9jf <67426585+eu9jf@users.noreply.github.com>
2020-07-13 12:30:26 -0400

    Added Achievement Gap Column

--- EC Data Set.R
+++ EC Data Set.R
@@ -38,6 +38,35 @@ write_csv(division,path = 'division.csv')
 #Albemarle
 div_alb <- division %>% 
   filter(Division.Number == '2') %>% 
+  filter(Division.Number == '2') %>%
+  filter(Pass.Count != "<")
+
+div_alb_black <- div_alb %>%
+  filter(Race == "Black, not of Hispanic origin") %>%
+  select(School.Year, Test.Level, Black_Pass_Count = Pass.Count, 
+         Black_Pass_Total = Total.Count, Black_Pass_Rate = Pass.Rate)
+
+div_alb_white <- div_alb %>%
+  filter(Race != "Black, not of Hispanic origin") %>%
+  select(School.Year, Test.Level, White_Pass_Count = Pass.Count, 
+         White_Pass_Total = Total.Count, White_Pass_Rate = Pass.Rate)
+
+div_alb <- div_alb %>%
+  select(School.Year, Test.Level) %>%
+  distinct() %>%
+  left_join(div_alb_black) %>%
+  left_join(div_alb_white) %>%
+  as.tibble() %>%
+  transform(White_Pass_Rate = as.numeric(as.character(White_Pass_Rate)), 
+            Black_Pass_Rate = as.numeric(as.character(Black_Pass_Rate))) %>%
+  mutate(Achievement_Gap = White_Pass_Rate - Black_Pass_Rate)
+  
+
+#, div_alb_white)
+
+
+
+>>>>>>> parent of 234da17... Achievement Gap column
   spread(Race,Pass.Rate)
 
 
